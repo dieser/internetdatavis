b5cfdab6d88e5409ba079e80dc3568a2bf9b4fd7
Dale <dalejchapman@gmail.com>
2016-05-10 16:33:23 +1000

    added parsing json data + multiple refactors. Still wip

--- js/main.js
+++ js/main.js
@@ -9,9 +9,11 @@
 var app = app || {};
 app.step = 0;
 
+app.testcube = {};
+app.jsondata = {};
 
 app.controller = {
-  rotationSpeed: 0.002
+  rotationSpeed: 0.0 //0.002
 };
 /////////////////////////////////////////////
 
@@ -23,6 +25,7 @@ app.animate = function(){
   app.stats.update();
 
   app.sphere.rotation.y += app.controller.rotationSpeed;
+  
 
   app.renderer.render( app.scene, app.camera );
 
@@ -74,7 +77,7 @@ app.init = function() {
 ////////////////////////////////////////
 
 
-  var sphereGeometry = new THREE.SphereGeometry(100, 60, 60);
+  var sphereGeometry = new THREE.SphereGeometry(100, 60, 60, 20);
   var sphereMaterial = new THREE.MeshLambertMaterial({
     color: 0xFFFFFF,
     map: THREE.ImageUtils.loadTexture("images/worldtexture.jpg")
@@ -113,8 +116,19 @@ app.init = function() {
 
 /////////////////////////////////////////////
 
-
-
+//
+// var lat = 28.165547;
+// var long = 71.185474;
+// var testpoint = latLongToVector3(lat, long, 100, 0.1);
+// console.log(testpoint);
+//
+// var cubeMat = new THREE.MeshLambertMaterial({color: 0xff0000, opacity:0.6});
+// var cube = new THREE.Mesh(new THREE.CubeGeometry(2, 2, 2));
+// cube.position.x = testpoint.x; //new THREE.Vector3(100, 100, 100); //testpoint;
+// cube.position.y = testpoint.y; //new THREE.Vector3(100, 100, 100); //testpoint;
+// cube.position.z = testpoint.z; //new THREE.Vector3(100, 100, 100); //testpoint;
+// app.scene.add(cube);
+// app.testcube = cube;
 
 ////////////////////////////////////////////
 //////////// Controls for orbit ///////////
@@ -129,6 +143,15 @@ app.init = function() {
   document.getElementById("output").appendChild( app.renderer.domElement );
 
   app.animate();
+
+  $.getJSON("/js/list.geo.json", function(json){
+
+    console.log(json);
+    app.jsondata = json;
+    addDensity(json);
+  });
+
+
 }
 /////////////////////////////////////////////
 
@@ -184,17 +207,79 @@ app.onResize = function(){
 window.addEventListener("resize", app.onResize);
 /////////////////////////////////////////////
 
+
+
+
 ////////////////////////////////////////////////////
 /// change from cartesian co-ords to long + lat ///
 //////////////////////////////////////////////////
-function latLongToVector3(lat, lon, radius, heigth) {
+function latLongToVector3(lat, lon, radius, height) {
     var phi = (lat)*Math.PI/180;
     var theta = (lon-180)*Math.PI/180;
 
-    var x = -(radius+heigth) * Math.cos(phi) * Math.cos(theta);
-    var y = (radius+heigth) * Math.sin(phi);
-    var z = (radius+heigth) * Math.cos(phi) * Math.sin(theta);
+    var x = -(radius+height) * Math.cos(phi) * Math.cos(theta);
+    var y = (radius+height) * Math.sin(phi);
+    var z = (radius+height) * Math.cos(phi) * Math.sin(theta);
 
     return new THREE.Vector3(x,y,z);
 }
 /////////////////////////////////////////////
+
+// simple function that converts the density data to the markers on screen
+// the height of each marker is relative to the density.
+function addDensity(data) {
+
+
+data = data.features[2].geometry.coordinates[0];
+//first loop: over data.features[]
+  //2nd loop: over data.features[n].geometry.coordinates[] - in case of multiple shapes
+
+      //third loop: over each set of points, as below: coordinates.length
+
+
+    // the geometry that will contain all our cubes
+    var geom = new THREE.Geometry();
+    // material to use for each of our elements. Could use a set of materials to
+    // add colors relative to the density. Not done here.
+    var cubeMat = new THREE.MeshLambertMaterial({color: 0x000000,opacity:0.6, emissive:0xffffff});
+    for (var i = 0 ; i < data.length-1 ; i++) {
+
+        //get the data, and set the offset, we need to do this since the x,y coordinates
+        //from the data aren't in the correct format
+        //var x = parseInt(data[i][0])+180;
+        //var y = parseInt((data[i][1])-84)*-1;
+        var value = 50; //parseFloat(data[i][2]);
+
+        //var lat = 28.165547;
+        //var long = 71.185474;
+
+        // calculate the position where we need to start the cube
+        var position = latLongToVector3(data[i][0], data[i][1], 100, 1);
+        // console.log(position, data[i][0], data[i][1]);
+        // create the cube
+        //var cube = new THREE.Mesh(new THREE.CubeGeometry(5,5,1+value/8,1,1,1,cubeMat));
+
+
+        var cubeMat = new THREE.MeshLambertMaterial({color: 0xff0000, opacity:0.6});
+        var cube = new THREE.Mesh(new THREE.CubeGeometry(1, 1, 1));
+        cube.position.x = position.x;
+        cube.position.y = position.y;
+        cube.position.z = position.z;
+        // position the cube correctly
+
+        //cube.lookAt( new THREE.Vector3(0,0,0) );
+
+        // merge with main model
+        cube.updateMatrix();
+        geom.merge(cube.geometry, cube.matrix);
+    }
+
+    // create a new mesh, containing all the other meshes.
+    var matty = new THREE.MeshPhongMaterial({color: 0xFF0000});
+    var total = new THREE.Mesh(geom, matty);
+
+    // and add the total mesh to the scene
+    //console.log(app.scene);
+
+    app.scene.add(total);
+}
